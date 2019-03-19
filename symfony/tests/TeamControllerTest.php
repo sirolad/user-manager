425e340b7cfc83628b9d699d2937230cfba93256
andela-sakande <surajudeen.akande@andela.com>
2019-03-19 12:04:24 +0100

    add functional tests, need to comment api setting in security.yml

--- symfony/tests/TeamControllerTest.php
+++ symfony/tests/TeamControllerTest.php
@@ -1,23 +1,22 @@
 <?php
-
 namespace App\Tests;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
-class AdminControllerTest extends WebTestCase
+class TeamControllerTest extends WebTestCase
 {
     public function testStoreUser()
     {
         $client = static::createClient();
-        $client->request('POST', '/admin', ['username' => 'tester', 'role' => 'user', 'password' => 'mall']);
+        $client->request('POST', '/team', ['name' => 'tester team']);
 
         $this->assertEquals(201, $client->getResponse()->getStatusCode());
     }
 
-    public function testDeleteUser()
+    public function testDeleteTeam()
     {
         $client = static::createClient();
-        $client->request('DELETE', '/admin/1');
+        $client->request('DELETE', '/team/1');
 
         $this->assertEquals(204, $client->getResponse()->getStatusCode());
     }
